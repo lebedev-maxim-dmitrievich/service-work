d45d9ce7252a105ed1296b7ed60c79b3899dc2f2
maxim-lebedev <cyue.alle@gmail.com>
2021-03-27 19:27:58 +0300

    init commit

---
+++ src/main/java/ru/lebedev/servicework/ServiceWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedev.servicework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWorkApplication.class, args);
+	}
+
+}
